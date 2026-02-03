30f5735324e447e7d4ca7decd7eb8a13d7173297
Alla Mozharova <alik19.09@mail.ru>
2026-02-04 00:56:56 +0300

    feat: добавить сервис получения расписания рейсов между станциями

---
+++ TravelSchedule/Services/ScheduleBetweenStationsService.swift
@@ -0,0 +1,27 @@
+import OpenAPIRuntime
+
+typealias Segments = Components.Schemas.Segments
+
+protocol ScheduleBetweenStationsServiceProtocol {
+    func getScheduleBetweenStations(
+        from: String,
+        to: String
+    ) async throws -> Segments
+}
+
+final class ScheduleBetweenStationsService: ScheduleBetweenStationsServiceProtocol {
+    private let client: Client
+    private let apikey: String
+    
+    init(client: Client, apikey: String) {
+        self.client = client
+        self.apikey = apikey
+    }
+    
+    func getScheduleBetweenStations(from: String, to: String) async throws -> Segments {
+        let response = try await client.getScheduleBetweenStations(
+            query: .init(apikey: apikey, from: from, to: to)
+        )
+        return try response.ok.body.json
+    }
+}
