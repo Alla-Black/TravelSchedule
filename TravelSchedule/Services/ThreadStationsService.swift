ca41ed9e198d7e915276d8060919e57eaa28d616
Alla Mozharova <alik19.09@mail.ru>
2026-02-04 17:06:34 +0300

    feat: добавить сервис получения списка станций следования нитки по идентификатору нитки

---
+++ TravelSchedule/Services/ThreadStationsService.swift
@@ -0,0 +1,25 @@
+import OpenAPIRuntime
+
+typealias ThreadStationsResponse = Components.Schemas.ThreadStationsResponse
+
+protocol ThreadStationsServiceProtocol {
+    func getRouteStations(uid: String) async throws -> ThreadStationsResponse
+}
+
+final class ThreadStationsService: ThreadStationsServiceProtocol {
+    private let client: Client
+    private let apikey: String
+    
+    init(client: Client, apikey: String) {
+        self.client = client
+        self.apikey = apikey
+    }
+    
+    func getRouteStations(uid: String) async throws -> ThreadStationsResponse {
+        let response = try await client.getRouteStations(query: .init(
+            apikey: apikey,
+            uid: uid
+        ))
+        return try response.ok.body.json
+    }
+}
