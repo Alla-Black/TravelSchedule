6196a521ffb43e1b733c0451e9ada7863c720e10
Alla Mozharova <alik19.09@mail.ru>
2026-02-04 18:10:34 +0300

    feat: добавить сервис получения информации о ближайшем к указанной точке городе

---
+++ TravelSchedule/Services/NearestCityService.swift
@@ -0,0 +1,28 @@
+import OpenAPIRuntime
+
+typealias NearestCityResponse = Components.Schemas.NearestCityResponse
+
+protocol NearestCityServiceProtocol {
+    func getNearestCity(lat: Double, lng: Double, distance: Int?) async throws -> NearestCityResponse
+}
+
+final class NearestCityService: NearestCityServiceProtocol {
+    private let client: Client
+    private let apikey: String
+    
+    init(client: Client, apikey: String) {
+        self.client = client
+        self.apikey = apikey
+    }
+    func getNearestCity(lat: Double, lng: Double, distance: Int?) async throws -> NearestCityResponse {
+        
+        let response = try await client.getNearestCity(query: .init(
+            apikey: apikey,
+            lat: lat,
+            lng: lng,
+            distance: distance
+        ))
+        
+        return try response.ok.body.json
+    }
+}
