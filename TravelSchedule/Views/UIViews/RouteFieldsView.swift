7b9454cff2d30625ff577d1abdc2d27122f36370
Alla Mozharova <alik19.09@mail.ru>
2026-02-13 15:40:30 +0300

    feat: добавить вёрстку полей ввода маршрута (RouteFieldsView)

---
+++ TravelSchedule/Views/UIViews/RouteFieldsView.swift
@@ -0,0 +1,49 @@
+import SwiftUI
+
+struct RouteFieldsView: View {
+    @State private var from: String = ""
+    @State private var to: String = ""
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            TextField(
+                "",
+                text: $from,
+                prompt: Text("Откуда")
+                    .font(.system(size: 17, weight: .regular))
+                    .foregroundStyle(Color.grayUniversal)
+            )
+            .font(.system(size: 17, weight: .regular))
+            .foregroundStyle(Color.blackUniversal)
+            .textFieldStyle(.plain)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.vertical, 14)
+            .padding(.horizontal, from.isEmpty ? 16 : 13)
+            
+            TextField(
+                "",
+                text: $to,
+                prompt: Text("Куда")
+                    .font(.system(size: 17, weight: .regular))
+                    .foregroundStyle(Color.grayUniversal)
+            )
+            .font(.system(size: 17, weight: .regular))
+            .foregroundStyle(Color.blackUniversal)
+            .textFieldStyle(.plain)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.vertical, 14)
+            .padding(.horizontal, to.isEmpty ? 16 : 13)
+        }
+        
+        .background(
+            RoundedRectangle(cornerRadius: 20)
+                .fill(Color.whiteUniversal)
+        )
+        .frame(maxWidth: .infinity)
+        
+    }
+}
+
+#Preview {
+    RouteFieldsView()
+}
