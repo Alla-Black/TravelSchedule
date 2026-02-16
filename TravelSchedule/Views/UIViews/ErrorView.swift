4ca23c56cb42373cf6dab4a63770c1f591f72271
Alla Mozharova <alik19.09@mail.ru>
2026-02-16 22:19:02 +0300

    feat: добавить общий экран ошибок ErrorView

---
+++ TravelSchedule/Views/UIViews/ErrorView.swift
@@ -0,0 +1,30 @@
+import SwiftUI
+
+struct ErrorView: View {
+ let state: AppErrorState
+    var body: some View {
+        ZStack {
+            Color.whiteDayNight
+                .ignoresSafeArea()
+            VStack(spacing: 16) {
+                Image(state.imageName)
+                    .resizable()
+                    .frame(width: 223, height: 223)
+                Text(state.title)
+                    .font(.system(size: 24, weight: .bold))
+                    .foregroundStyle(.blackDayNight)
+                    .multilineTextAlignment(.center)
+                    .frame(maxWidth: .infinity)
+                    
+            }
+        }
+    }
+}
+
+#Preview {
+    ErrorView(state: .noInternet)
+}
+
+#Preview {
+    ErrorView(state: .server)
+}
