d595ccb7242f2d19364f33ba97ef019416bbc58f
Alla Mozharova <alik19.09@mail.ru>
2026-02-21 19:01:38 +0300

    feat: доработать инициализацию модели ScheduleCardItem

--- TravelSchedule/Domain Models/ScheduleCardItem.swift
+++ TravelSchedule/Domain Models/ScheduleCardItem.swift
@@ -12,4 +12,28 @@ struct ScheduleCardItem: Identifiable, Hashable {
     let durationTitle: String
     
     let transfersTitle: String?
+    
+    init(
+        uid: String,
+        departureRawString: String,
+        carrierTitle: String,
+        carrierLogoURL: URL?,
+        dateTitle: String,
+        departureTimeTitle: String,
+        arrivalTimeTitle: String,
+        durationTitle: String,
+        hasTransfers: Bool
+    ) {
+        self.id = "\(uid)_\(departureRawString)"
+        
+        self.carrierTitle = carrierTitle
+        self.carrierLogoURL = carrierLogoURL
+        
+        self.dateTitle = dateTitle
+        self.departureTimeTitle = departureTimeTitle
+        self.arrivalTimeTitle = arrivalTimeTitle
+        self.durationTitle = durationTitle
+        
+        self.transfersTitle = hasTransfers ? "С пересадкой" : nil
+    }
 }
