2b9c220c97f71e7aa3577b59a09e4ff25dbad94d
Alla Mozharova <alik19.09@mail.ru>
2026-03-06 14:41:42 +0300

    feat: сверстать экран настроек SettingsScreen

--- TravelSchedule/Views/Screens/SettingsScreen.swift
+++ TravelSchedule/Views/Screens/SettingsScreen.swift
@@ -1,11 +1,66 @@
 import SwiftUI
 
 struct SettingsScreen: View {
+    private enum Constants {
+        static let themeSettings = "Темная тема"
+        static let userAgreement = "Пользовательское соглашение"
+        static let apiImplementation = "Приложение использует API «Яндекс.Расписания»"
+    }
+    
+    private var appVersionText: String {
+        let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "—"
+        return "Версия \(version) (beta)"
+    }
+    
     var body: some View {
         ZStack {
             Color.whiteDayNight
                 .ignoresSafeArea()
-            Text("Here will be the settings screen")
+            
+            VStack {
+                VStack(alignment: .leading, spacing: 0) {
+                    HStack {
+                        Text(Constants.themeSettings)
+                            .font(.system(size: 17, weight: .regular))
+                            .foregroundStyle(.blackDayNight)
+                            .padding(.vertical, 19)
+                        Spacer()
+                        Toggle("", isOn: .constant(false))
+                            .tint(.blueUniversal)
+                            .padding(.leading, 4)
+                    }
+                    
+                    Button {
+                        
+                    } label: {
+                        HStack {
+                            Text(Constants.userAgreement)
+                                .font(.system(size: 17, weight: .regular))
+                                .foregroundStyle(.blackDayNight)
+                                .padding(.vertical, 19)
+                            Spacer()
+                            Image(.shevron)
+                                .frame(width: 24, height: 24)
+                                .foregroundStyle(.blackDayNight)
+                                .padding(.leading, 4)
+                        }
+                    }
+                    .frame(maxWidth: .infinity)
+                }
+                .padding(.top, 24)
+                
+                Spacer()
+                
+                VStack(alignment: .center, spacing: 16) {
+                    Text(Constants.apiImplementation)
+                    Text(appVersionText)
+                }
+                .font(.system(size: 12, weight: .regular))
+                .foregroundStyle(.blackDayNight)
+                .multilineTextAlignment(.center)
+                .padding(.bottom, 24)
+            }
+            .padding(.horizontal, 16)
         }
     }
 }
